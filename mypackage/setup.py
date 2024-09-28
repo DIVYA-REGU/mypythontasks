bffc9be06d79dd04647c7fd5d0456826e8e2b87f
DIVYA-REGU <rdivya.velusamy@gmail.com>
2024-09-28 10:10:33 +0530

    Add files via upload

---
+++ mypackage/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1.0",
+    description="A utility package for math, string, and file operations",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
