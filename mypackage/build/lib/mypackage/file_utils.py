a058b4895fe9e5f4ac1d45bf73dc11a181059190
DIVYA-REGU <rdivya.velusamy@gmail.com>
2024-09-30 11:21:06 +0530

    Delete mypackage directory

--- mypackage/build/lib/mypackage/file_utils.py
+++
@@ -1,7 +0,0 @@
-def read_file(filepath):
-    with open(filepath, 'r') as f:
-        return f.read()
-
-def write_file(filepath, content):
-    with open(filepath, 'w') as f:
-        f.write(content)
