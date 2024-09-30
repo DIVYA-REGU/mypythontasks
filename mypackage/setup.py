e1cddf9b10ae29ee3d6d54be9042bd2eccfe6cec
DIVYA-REGU <rdivya.velusamy@gmail.com>
2024-09-30 17:45:17 +0530

    Add files via upload

---
+++ mypackage/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Divyaregu",
+    version="0.1.3",
+    description="A utility package for math, string, and file operations",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
